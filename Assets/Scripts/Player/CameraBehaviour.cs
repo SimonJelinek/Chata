69a6ab03af3036f336efc17895b78dd46f3ad31b
GoldenPants64 <martinklacan111@gmail.com>
2021-04-16 00:34:21 +0200

    Added camera movement

---
+++ Assets/Scripts/Player/CameraBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Camera cam;
+    private float xOffset;
+    private float yOffset;
+    private float zOffset;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        xOffset = 0f;
+        yOffset = 0.1f;
+        zOffset = -5f;
+        offset = new Vector3(xOffset, yOffset, zOffset);
+    }
+    private void Update()
+    {
+        Vector3 playerPosition = transform.position;
+        Vector3 newCamPosition = playerPosition + offset;
+        cam.transform.position = newCamPosition;
+    }
+}
