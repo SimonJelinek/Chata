7c9bf74b645cc3a0acf7085fa3df36c6a845d7a1
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-17 00:02:47 +0200

    Added simple settings screen

--- Assets/Scripts/UI/ScreenSwitch.cs
+++ Assets/Scripts/UI/ScreenSwitch.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class ScreenSwitch : MonoBehaviour
 {
+    void Start()
+    {
+        App.screenManager.Show<MenuScreen>();
+    }
+
     public void ShowSettings()
     {
         App.screenManager.Hide<MenuScreen>();
         App.screenManager.Show<SettingsScreen>();
+    }
+
+    public void ReturnToMenu()
+    {
+        App.screenManager.Hide<SettingsScreen>();
+        App.screenManager.Show<MenuScreen>();
     }
 }
