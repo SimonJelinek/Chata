f5e3a6ed9130230b2c3930b591ef97aed62d8f43
salinger11 <ssalinger11@gmail.com>
2021-04-18 19:10:06 +0200

    Added Weapon Variety

--- Assets/Scripts/Player/Shooting/Shooting.cs
+++ Assets/Scripts/Player/Shooting/Shooting.cs
@@ -12,22 +12,44 @@ public class Shooting : MonoBehaviour
     public TextMeshProUGUI _ammunitionDisplay;
    // public TextMeshProUGUI _reloadingText;
 
-    [Header("Magazine")]
+    [Header("Weapon Types")]
 
     [SerializeField] private int _allBullets;
-    [SerializeField] private int _magazineSize = 2;
+    [SerializeField] private int _magazineSize;
     [SerializeField] private int _bulletsLeft;
     [SerializeField] private float _reloadTime;
+    [SerializeField] private bool _allowButtonHold;
+    [SerializeField] private float _timeBetweenShooting;
+    [SerializeField] private float _timeBetweenShots;
+    [SerializeField] private int _bulletsPerTap;
+    [SerializeField] private float _spread;
+    private bool _shooting;
     private bool _reloading = false;
+    private bool _allowInvoke = true;
+    private bool _readyToShoot = true;
+    private int _bulletsShot;
 
     [Header("Bullet Variables")]
 
     [SerializeField] private float _bulletForce = 20f;
 
+
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1") && _bulletsLeft > 0)
+        if(_allowButtonHold == true)
         {
+            _shooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            _shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+       
+
+        if (_readyToShoot && _shooting && _bulletsLeft > 0 && _reloading == false)
+        {
+            _bulletsShot = 0;
+
             Shoot();
         }
 
@@ -42,7 +64,7 @@ public class Shooting : MonoBehaviour
 
         if (_ammunitionDisplay != null)
         {
-            _ammunitionDisplay.SetText(_bulletsLeft + " / " + _allBullets);
+            _ammunitionDisplay.SetText(_bulletsLeft / _bulletsPerTap + " / " + _allBullets / _bulletsPerTap);
         }
 
         /*if(_reloading)
@@ -62,16 +84,38 @@ public class Shooting : MonoBehaviour
 
     private void Shoot()
     {
-        if(_reloading == false)
-        {
+            _readyToShoot = false;
+        
             GameObject _bullet = Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
 
+            float x = Random.Range(-_spread, _spread);
+            float y = Random.Range(-_spread, _spread);
+
             Rigidbody2D _rb = _bullet.GetComponent<Rigidbody2D>();
-            _rb.AddForce(_firePoint.right * _bulletForce, ForceMode2D.Impulse);
+            _rb.AddForce(_firePoint.right * _bulletForce + new Vector3(x, y, 0), ForceMode2D.Impulse);
 
             _bulletsLeft--;
-        }
+            _bulletsShot++;
+
+            if (_allowInvoke)
+            {
+                Invoke("ResetShot", _timeBetweenShooting);
+                _allowInvoke = false;
+            }
+
+            if(_bulletsShot < _bulletsPerTap && _bulletsLeft > 0)
+            {
+                Invoke("Shoot", _timeBetweenShots);
+            }
+         
     }
+
+    private void ResetShot()
+    {
+        _readyToShoot = true;
+        _allowInvoke = true;
+    }
+
 
     private void Reload()
     {
