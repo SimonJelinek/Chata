4ab57edda519d19828889a361aa8abefddf3e1df
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-24 16:36:19 +0200

    Added Raycasting

---
+++ Assets/Scripts/Enemies/StaticEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemy : MonoBehaviour
+{
+    public GameObject bullet;
+    public float hitDistance;
+
+    void FixedUpdate()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.left, hitDistance))
+        {
+            Debug.Log("hit");
+        }
+
+        Debug.DrawRay(transform.position, Vector2.left * hitDistance, Color.red);
+    }
+}
