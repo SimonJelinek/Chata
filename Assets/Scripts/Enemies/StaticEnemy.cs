7d41e2306cee29932f76806a8640842cce3c11ff
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-24 16:50:23 +0200

    Added StaticEnemyBullet Script&Prefab

--- Assets/Scripts/Enemies/StaticEnemy.cs
+++ Assets/Scripts/Enemies/StaticEnemy.cs
@@ -6,12 +6,34 @@ public class StaticEnemy : MonoBehaviour
 {
     public GameObject bullet;
     public float hitDistance;
+    public float spawnTime;
+    float time;
+
+    bool shooting = false;
+
+    void Update()
+    {
+        if (shooting)
+        {
+            time += Time.deltaTime;
+
+            if (time >= spawnTime)
+            {
+                Instantiate(bullet, new Vector2(3.9f, 0.19f), Quaternion.identity);
+                time = 0;
+            }
+        }
+    }
 
     void FixedUpdate()
     {
         if (Physics2D.Raycast(transform.position, Vector2.left, hitDistance))
         {
-            Debug.Log("hit");
+            shooting = true;
+        }
+        else
+        {
+            shooting = false;
         }
 
         Debug.DrawRay(transform.position, Vector2.left * hitDistance, Color.red);
