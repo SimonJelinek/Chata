8ad4ee520dffd6bb2b03f92520b5a6fe9efbfe96
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-05-08 15:54:37 +0200

    Added Checkpoints script, checkpoints object

---
+++ Assets/Scripts/Player/Checkpoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    Vector2 checkPoint;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
