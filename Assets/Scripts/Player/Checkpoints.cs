bf6f2fe27120097b5e9eae4d55ef040227379603
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-05-08 16:08:28 +0200

    Added CheckPoints to prefabs
    Objekt s checkpointami som dal do Prefabov a script do medzi Player scripty

--- Assets/Scripts/Player/Checkpoints.cs
+++ Assets/Scripts/Player/Checkpoints.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Checkpoints : MonoBehaviour
 {
-    Vector2 checkPoint;
+    public Vector2 checkPoint;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Checkpoint1")
+        {
+            checkPoint = new Vector2(3.63f, -9.86f);
+        }
+
+        if (collision.gameObject.tag == "Checkpoint2")
+        {
+            checkPoint = new Vector2(-10, -2.46f);
+        }
+
+        if (collision.gameObject.tag == "Checkpoint3")
+        {
+            checkPoint = new Vector2(-3, 8.9f);
+        }
     }
 }
