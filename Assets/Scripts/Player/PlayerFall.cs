1e33cd8716976d09dac9888e1003fc86071acd89
GoldenPants64 <martinklacan111@gmail.com>
2021-06-25 23:24:44 +0200

    Coded player fall in Tutorial

---
+++ Assets/Scripts/Player/PlayerFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFall : MonoBehaviour
+{
+    public GameObject[] dosky = new GameObject[6];
+    public GameObject podlaha;
+
+    public BoxCollider2D podlahaCollider;
+    public PolygonCollider2D doska1Collider;
+    public PolygonCollider2D doska2Collider;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Doska"))
+        {
+            podlahaCollider.isTrigger = true;
+
+            foreach(GameObject doska in dosky)
+            {
+                doska.AddComponent<Rigidbody2D>();
+            }
+
+            doska1Collider.isTrigger = false;
+            doska2Collider.isTrigger = false;
+        }
+    }
+
+}
