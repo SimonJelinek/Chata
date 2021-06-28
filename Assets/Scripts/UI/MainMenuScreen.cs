a6ea423eeac0c057d384493eedbd45be272ec8f0
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-06-28 21:51:52 +0200

    Updated MenuScene

--- Assets/Scripts/UI/MainMenuScreen.cs
+++ Assets/Scripts/UI/MainMenuScreen.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScreen : MonoBehaviour
 {
+    public GameObject[] buttons;
     public Slider volumeSlider;
 
     public GameObject settingsScreen;
@@ -19,23 +20,39 @@ public class MainMenuScreen : MonoBehaviour
     public void ShowLevels() 
     {
         levelsScreen.SetActive(true);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetActive(false);
+        }
     }
 
     public void HideLevels() 
     {
         levelsScreen.SetActive(false);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetActive(true);
+        }
     }
 
     public void ShowSettings()
     {
         settingsScreen.SetActive(true);
         volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetActive(false);
+        }
     }
 
     public void GoBack()
     {
         settingsScreen.SetActive(false);
         PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetActive(true);
+        }
     }
 
     public void QuitGame()
