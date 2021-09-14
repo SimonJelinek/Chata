bd7927c34600d33cda108f73c3f9b97e02da0194
GoldenPants64 <martinklacan111@gmail.com>
2021-09-14 21:01:24 +0200

    Fixed dissolve on new following enemy

---
+++ Assets/Scripts/Enemies/Dissolve_FollowingEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve_FollowingEnemy : MonoBehaviour
+{
+    public EnemyFollowAI_NEW followingEnemy;
+    public SpriteRenderer[] childrenSr;
+
+    private float timer;
+
+    private void Start()
+    {
+        childrenSr = GetComponentsInChildren<SpriteRenderer>();
+        timer = 0.75f;
+    }
+    private void Update()
+    {
+        if(followingEnemy.health <= 0)
+        {
+
+            foreach(SpriteRenderer bodyPart in childrenSr)
+            {
+                bodyPart.material.SetFloat("Fade", timer / 0.75f);
+            }
+
+            timer -= Time.deltaTime;
+        }
+    }
+    
+}
